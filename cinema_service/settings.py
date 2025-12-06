4ffc0419de7262bf2aa92052aca482b6c79a39bc
Thaís <parisethais@gmail.com>
2025-12-06 06:04:41 -0300

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -142,6 +142,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
